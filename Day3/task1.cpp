b5fe1e12d7412773dc580c3b34c6b26395a9db8f
frolushka <mpfrolovstud@gmail.com>
2017-07-12 00:12:31 +0300

    Fixed radix sort

--- Day3/task1.cpp
+++ Day3/task1.cpp
@@ -3,6 +3,7 @@
 // Разработано в CLion 2017.1.3
 
 #include <ctime>
+#include <limits>
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -10,12 +11,13 @@ using namespace std;
 void print(int*, int);
 void swap(int*, int, int);
 void counting_sort(int*, int, int, int);
+void radix_sort(int*, int, int, int);
 int* array_copy(int*, int);
 
 int main() {
     srand(time(0));
 
-    int n, min = INT_MAX, max = 0; // Размер массива и максимальный элемент массива.
+    int n, min = INT8_MAX, max = 0; // Размер массива и максимальный элемент массива.
 
     // Определяем, как задавать элементы массива: из файла или с помощью
     // генератора случайных чисел. Некорректный ввод не учитываем.
@@ -60,7 +62,8 @@ int main() {
     }
 
     counting_sort(array_copy(arr, n), n, min, max);
-
+    radix_sort(array_copy(arr, n), n, min, max);
+    
     return 0;
 }
 
@@ -118,6 +121,62 @@ void counting_sort(int* arr, int n, int min, int max) {
     arr = sorted;
 
     cout << "Array after counting sort (stable): " << endl;
+    print(arr, n);
+    cout << "Iterations done: " << icount << endl;
+    cout << "Swaps done: " << scount << endl;
+}
+
+/// <summary>
+/// Цифровая сортировка.
+/// </summary>
+void radix_sort(int* arr, int n, int min, int max) {
+    union uint_256 {
+        unsigned int num;
+        char chars[4];
+    };
+
+    cout << "------------------------" << endl;
+    cout << "Array before radix sort (with union): " << endl;
+    print(arr, n);
+
+    // Подготовка.
+    uint_256* uarr = new uint_256[n];
+    for(int i = 0; i < n; i++) {
+        uarr[i].num = (unsigned int)arr[i];
+    }
+
+    int scount = 0; // Количество свапов.
+    int icount = 0; // Колличество итераций алгоритма.
+
+    int* counter = new int[max - min + 1];
+    uint_256* sorted = new uint_256[n];
+
+    for (int dig = 3; dig >= 0; dig--) {
+        for (int i = 0; i < max - min + 1; i++) {
+            icount++;
+            counter[i] = 0;
+        }
+        for (int i = 0; i < n; i++){
+            icount++;
+            counter[uarr[i].chars[dig] - min]++;
+        }
+        for (int j = 1; j < max - min + 1; j++) {
+            icount++;
+            counter[j] += counter[j - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            icount++;
+            counter[uarr[i].chars[dig] - min]--;
+            scount++;
+            sorted[counter[uarr[i].chars[dig] - min]] = uarr[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = sorted[i].num;
+    }
+
+    cout << "Array after radix sort (with union): " << endl;
     print(arr, n);
     cout << "Iterations done: " << icount << endl;
     cout << "Swaps done: " << scount << endl;
